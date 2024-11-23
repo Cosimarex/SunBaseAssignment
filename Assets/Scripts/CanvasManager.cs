3b687ae717efe44f1b4768fefb2d61cda15c11c4
Vishnu <84490319+Cosimarex@users.noreply.github.com>
2024-11-23 17:57:47 +0530

    List Dropdown
    List Dropdown - Added dropdown and its functionality
    
    Remaining tasks for Task 1
    PopUp

--- Assets/Scripts/CanvasManager.cs
+++ Assets/Scripts/CanvasManager.cs
@@ -2,29 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
 public class CanvasManager : MonoBehaviour
 {
+
+    public enum Filter
+    {
+        AllClients,
+        Managers,
+        NonManagers
+    };
+
+
     public API_Manager APIManager;
     public GameObject ListPrefab,ListContent;
     APIResponse APIData;
-
+    public Filter ClientsFilter;
     public void ClearList()
     {
         for (int i = ListContent.transform.childCount - 1; i >= 0; i--)
         {
             Destroy(ListContent.transform.GetChild(i).gameObject);
         }
-        APIManager.FetchData();
     }
     public void RefetchData()
     {
         ClearList();
+        APIManager.FetchData();
     }
     public void DataFetched(APIResponse RecievedData)
     {
         APIData = RecievedData;
 
-        if (RecievedData == null || RecievedData.clients == null)//Making sure datais valid
+        if (RecievedData == null || RecievedData.clients == null)// Making sure datais valid
         {
             Debug.LogError("No data available to display.");
             return;
@@ -44,6 +56,48 @@ public class CanvasManager : MonoBehaviour
                 ClientData clientData = RecievedData.data.ContainsKey(client.id+"") ? RecievedData.data[client.id+""] : null;
 
                 // Set the data in the ListItem script
+                listItemScript.SetData(client, clientData);
+            }
+        }
+    }
+    public void ListClients()
+    {
+        // Clear existing prefabs
+        ClearList();
+
+        List<Client> filteredClients = new List<Client>();// Making Empty List
+
+        switch (ClientsFilter)
+        {
+            case Filter.AllClients:
+                filteredClients = APIData.clients; // Show all clients
+                break;
+
+            case Filter.Managers:
+                filteredClients = APIData.clients.FindAll(client => client.isManager); // Showing Only managers
+                break;
+
+            case Filter.NonManagers:
+                filteredClients = APIData.clients.FindAll(client => !client.isManager); // Showing Only non-managers
+                break;
+
+            default:
+                Debug.LogWarning("Unknown filter applied.");
+                return;
+        }
+
+        // Adding Filtered clients to list
+        foreach (Client client in filteredClients)
+        {
+            GameObject listItem = Instantiate(ListPrefab, ListContent.transform);
+            ListItem listItemScript = listItem.GetComponent<ListItem>();
+
+            if (listItemScript != null)
+            {
+                // Retrieve client data from the dictionary
+                APIData.data.TryGetValue(client.id+"", out ClientData clientData);
+
+                // Set data in the ListItem script
                 listItemScript.SetData(client, clientData);
             }
         }
---
+++ Assets/Scripts/DropDown.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class DropDown : MonoBehaviour
+{
+    public CanvasManager CanvasManager;
+
+    public void OnDropdownValueChanged(int value)
+    {
+        switch (value)
+        {
+            case 0:
+                CanvasManager.ClientsFilter = CanvasManager.Filter.AllClients;
+                break;
+            case 1:
+                CanvasManager.ClientsFilter = CanvasManager.Filter.Managers;
+                break;
+            case 2:
+                CanvasManager.ClientsFilter = CanvasManager.Filter.NonManagers;
+                break;
+            default:
+                Debug.LogWarning("Unknown dropdown value selected.");
+                return;
+        }
+
+        // Update the client list based on the selected filter
+        CanvasManager.ListClients();
+    }
+}
